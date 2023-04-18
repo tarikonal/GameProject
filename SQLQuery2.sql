64db34c43182d09474b53950ef747dcfa645db00
tarik.onal <tarik.onal@csgb.gov.tr>
2023-04-18 15:38:04 +0300

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,18 @@
+﻿--select * from Products order by UnitPrice
+
+select count (*) as ToplamUrun, p.CategoryID, c.CategoryName,p.UnitPrice 
+from Products as p 
+inner join Categories as c on p.CategoryID = c.CategoryID
+--where UnitPrice>20
+--group by p.CategoryID, c.CategoryName 
+having count(*)<10
+
+
+select * from Products as p 
+inner join [Order Details] as od on p.ProductID=od.ProductID
+inner join Orders o on o.OrderID=od.OrderID
+
+select c.* from Customers as c
+left join [Orders] as o on c.CustomerID=o.CustomerID
+where o.CustomerID is null
+
